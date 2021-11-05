3b40fbec9dc2f31ef2ef44a4f5b42e35f8e11e1d
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-11-05 14:08:40 +0100

    Mark the lowest/highest number

--- ReorderDatabase.Web/ClientApp/src/app/app.component.ts
+++ ReorderDatabase.Web/ClientApp/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { map, ReplaySubject, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Note } from './entities/Note';
 import { NoteService } from './services/note/note.service';
 
@@ -11,19 +15,67 @@ import { NoteService } from './services/note/note.service';
 export class AppComponent {
 
   constructor(private noteService: NoteService) {
+    this.numerators$ = this.notes$
+      .pipe(map(notes => {
+        return notes.map(note => note.numerator);
+      }));
+
+    this.largest$ = this.numerators$
+      .pipe(map(numerators => {
+        return numerators.sort((a, b) => {
+          if (a < b) {
+            return 1;
+          } else if (a === b) {
+            return 0;
+          } else {
+            return -1;
+          }
+        });
+      })).pipe(map(numerators => {
+        if (numerators.length < 2) {
+          return [0, 0];
+        } else {
+          return numerators.slice(0, 2);
+        }
+      }));
+
+    this.smallest$ = this.numerators$
+      .pipe(map(numerators => {
+        return numerators.sort((a, b) => {
+          if (a > b) {
+            return 1;
+          } else if (a === b) {
+            return 0;
+          } else {
+            return -1;
+          }
+        });
+      })).pipe(map(numerators => {
+        if (numerators.length === 0) {
+          return 0;
+        } else {
+          return numerators[0];
+        }
+      }));
+
+
     this.noteService.getNotes().subscribe((notes) => {
-      this.notes = notes;
+      this.notes$.next(notes);
     });
+
   }
 
   title = 'ClientApp';
-  notes: Note[] = [];
+  notes$ = new BehaviorSubject<Note[]>([]);
+  numerators$: Observable<number[]>;
+  largest$: Observable<number[]>;
+  smallest$: Observable<number>;
   newNoteText: string = '';
   numberOfSwaps: number = 0;
 
   addNote(text: string) {
     this.noteService.createNote(text).subscribe((note) => {
-      this.notes.push(note);
+      this.notes$.pipe(tap(notes => notes.push(note))).subscribe(() => null);
     });
   }
 
@@ -35,14 +87,21 @@ export class AppComponent {
         event.previousIndex,
         event.currentIndex);
 
-      console.log('event', event);
+      //console.log('event', event);
 
       this.noteService.swapNote(
-        event.currentIndex === 0 ? null : this.notes[event.currentIndex - 1],
+        event.currentIndex === 0 ? null : this.notes$.value[event.currentIndex - 1],
         <Note>event.container.data[event.currentIndex],
-        event.currentIndex === event.container.data.length - 1 ? null : this.notes[event.currentIndex + 1]
+        event.currentIndex === event.container.data.length - 1 ? null : this.notes$.value[event.currentIndex + 1]
       ).subscribe((note) => {
-        this.notes[event.currentIndex] = note;
+        let newState = this.notes$.value.map((item, index) => {
+          if (index === event.currentIndex) {
+            return note;
+          } else {
+            return item;
+          }
+        });
+        this.notes$.next(newState);
       });
     } else {
       transferArrayItem(
